7d20f27d6b4dc7be7347e673bac3653233ab8b53
Jan Böhmer <mail@jan-boehmer.de>
2022-07-10 02:24:22 +0200

    Use own REPL task instead of esp-idf example one.

--- src/ESP32Console/Console.cpp
+++ src/ESP32Console/Console.cpp
@@ -4,10 +4,11 @@
 #include "ESP32Console/Commands/CoreCommands.h"
 #include "ESP32Console/Commands/SystemCommands.h"
 #include "ESP32Console/Commands/NetworkCommands.h"
+#include "driver/uart.h"
+#include "esp_vfs_dev.h"
+#include "linenoise/linenoise.h"
 
 static const char *TAG = "ESP32Console";
-
-
 
 using namespace ESP32Console::Commands;
 
@@ -27,8 +28,30 @@ namespace ESP32Console
         registerCommand(getMemInfoCommand());
     }
 
-    void ESP32Console::Console::registerNetworkCommands() {
+    void ESP32Console::Console::registerNetworkCommands()
+    {
         registerCommand(getPingCommand());
+    }
+
+    void Console::beginCommon()
+    {
+        /* Tell linenoise where to get command completions and hints */
+        linenoiseSetCompletionCallback(&esp_console_get_completion);
+        linenoiseSetHintsCallback((linenoiseHintsCallback *)&esp_console_get_hint);
+
+        /* Set command history size */
+        linenoiseHistorySetMaxLen(max_history_len_);
+
+        /* Set command maximum length */
+        linenoiseSetMaxLineLen(max_history_len_);
+
+        /* Don't return empty lines */
+        linenoiseAllowEmpty(false);
+
+
+        // Register core commands like echo
+        esp_console_register_help_command();
+        registerCoreCommands();
     }
 
     void Console::begin(int baud, int rxPin, int txPin, uint8_t channel)
@@ -41,41 +64,142 @@ namespace ESP32Console
             return;
         }
 
-        // Register help command
-        esp_console_register_help_command();
+        /* Drain stdout before reconfiguring it */
+        fflush(stdout);
+        fsync(fileno(stdout));
 
-        // Configure the UART
-        esp_console_dev_uart_config_t hw_config = {
-            .channel = channel,
+        /* Disable buffering on stdin */
+        setvbuf(stdin, NULL, _IONBF, 0);
+
+        /* Minicom, screen, idf_monitor send CR when ENTER key is pressed */
+        esp_vfs_dev_uart_port_set_rx_line_endings(channel, ESP_LINE_ENDINGS_CR);
+        /* Move the caret to the beginning of the next line on '\n' */
+        esp_vfs_dev_uart_port_set_tx_line_endings(channel, ESP_LINE_ENDINGS_CRLF);
+
+        /* Configure UART. Note that REF_TICK is used so that the baud rate remains
+         * correct while APB frequency is changing in light sleep mode.
+         */
+        const uart_config_t uart_config = {
             .baud_rate = baud,
-            .tx_gpio_num = txPin,
-            .rx_gpio_num = rxPin};
+            .data_bits = UART_DATA_8_BITS,
+            .parity = UART_PARITY_DISABLE,
+            .stop_bits = UART_STOP_BITS_1,
+#if SOC_UART_SUPPORT_REF_TICK
+            .source_clk = UART_SCLK_REF_TICK,
+#elif SOC_UART_SUPPORT_XTAL_CLK
+            .source_clk = UART_SCLK_XTAL,
+#endif
+        };
+        /* Install UART driver for interrupt-driven reads and writes */
+        ESP_ERROR_CHECK(uart_driver_install(channel,
+                                            256, 0, 0, NULL, 0));
+        ESP_ERROR_CHECK(uart_param_config(channel, &uart_config));
 
-        auto code = esp_console_new_repl_uart(&hw_config, &repl_config_, &repl_);
-        if (code != ESP_OK)
-        {
-            log_e("Could not initialize UART. Check if you are still have an active Serial component! (Reason %s)", esp_err_to_name(code));
-            return;
+        /* Tell VFS to use UART driver */
+        esp_vfs_dev_uart_use_driver(channel);
+
+        esp_console_config_t console_config = {
+            .max_cmdline_length = max_cmdline_len_,
+            .max_cmdline_args = max_cmdline_args_,
+            .hint_color = 333333
+        };
+
+        ESP_ERROR_CHECK(esp_console_init(&console_config));
+
+        /* Change standard input and output of the task if the requested UART is
+         * NOT the default one. This block will replace stdin, stdout and stderr.
+         */
+        if (channel != CONFIG_ESP_CONSOLE_UART_NUM) {
+            char path[12] = { 0 };
+            snprintf(path, 12, "/dev/uart/%d", channel);
+
+            stdin = fopen(path, "r");
+            stdout = fopen(path, "w");
+            stderr = stdout;
         }
 
-        code = esp_console_start_repl(repl_);
-        if (code != ESP_OK)
+        beginCommon();
+
+        // Start REPL task
+        if (xTaskCreate(&Console::repl_task, "console_repl", 4096, this, 2, &task_) != pdTRUE)
         {
-            log_e("Could not start REPL! (Reason %s)", esp_err_to_name(code));
-            return;
+            log_e("Could not start REPL task!");
+        }
+    }
+
+    void Console::repl_task(void *args)
+    {
+        Console &console = *(static_cast<Console *>(args));
+
+        setvbuf(stdin, NULL, _IONBF, 0);
+
+        /* This message shall be printed here and not earlier as the stdout
+         * has just been set above. */
+        printf("\r\n"
+               "Type 'help' to get the list of commands.\r\n"
+               "Use UP/DOWN arrows to navigate through command history.\r\n"
+               "Press TAB when typing command name to auto-complete.\r\n");
+
+        //Probe terminal status
+        int probe_status = linenoiseProbe();
+        if(probe_status) {
+            linenoiseSetDumbMode(1);
         }
 
-        // Register core commands like echo
-        registerCoreCommands();
+        if (linenoiseIsDumbMode())
+        {
+            printf("\r\n"
+                   "Your terminal application does not support escape sequences.\n\n"
+                   "Line editing and history features are disabled.\n\n"
+                   "On Windows, try using Putty instead.\r\n");
+        }
+
+        linenoiseSetMaxLineLen(0);
+        while (1)
+        {
+            char *line = linenoise(console.prompt_);
+            if (line == NULL)
+            {
+                ESP_LOGD(TAG, "empty line");
+                /* Ignore empty lines */
+                continue;
+            }
+            /* Add the command to the history */
+            linenoiseHistoryAdd(line);
+            /* Save command history to filesystem */
+            if (console.history_save_path_)
+            {
+                linenoiseHistorySave(console.history_save_path_);
+            }
+
+            /* Try to run the command */
+            int ret;
+            esp_err_t err = esp_console_run(line, &ret);
+            if (err == ESP_ERR_NOT_FOUND)
+            {
+                printf("Unrecognized command\n");
+            }
+            else if (err == ESP_ERR_INVALID_ARG)
+            {
+                // command was empty
+            }
+            else if (err == ESP_OK && ret != ESP_OK)
+            {
+                printf("Command returned non-zero error code: 0x%x (%s)\n", ret, esp_err_to_name(ret));
+            }
+            else if (err != ESP_OK)
+            {
+                printf("Internal error: %s\n", esp_err_to_name(err));
+            }
+            /* linenoise allocates line buffer on the heap, so need to free it */
+            linenoiseFree(line);
+        }
+        ESP_LOGD(TAG, "REPL task ended");
+        vTaskDelete(NULL);
     }
 
     void Console::end()
     {
-        if (repl_)
-        {
-            // Stop REPL
-            repl_->del(repl_);
-            esp_console_deinit();
-        }
+
     }
 };
--- src/ESP32Console/Console.h
+++ src/ESP32Console/Console.h
@@ -7,43 +7,45 @@
 #include "esp_console.h"
 #include "Arduino.h"
 #include "./ConsoleCommandBase.h"
+#include "freertos/task.h"
+#include "linenoise/linenoise.h"
 
 namespace ESP32Console
 {
     class Console
     {
     private:
-        esp_console_repl_config_t repl_config_;
-        esp_console_repl_t *repl_ = nullptr;
+        const char *prompt_ = "ESP32> ";
+        const uint32_t task_priority_;
+        const BaseType_t task_stack_size_;
+
+        uint16_t max_history_len_ = 40;
+        const char* history_save_path_ = nullptr;
+
+        const size_t max_cmdline_len_;
+        const size_t max_cmdline_args_;
+
+        TaskHandle_t task_;
+
+        static void repl_task(void *args);
+
+        void beginCommon();
 
     public:
         /**
          * @brief Create a new ESP32Console with the default parameters
          */
-        Console()
-        {   
-            repl_config_ = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
-            repl_config_.prompt = "ESP32>";
-        }
-
-        /**
-         * @brief Create a new ESP32Console with the given configuration. Useful for advanced configurations
-         *
-         * @param config
-         */
-        Console(esp_console_repl_config_t config)
-        {
-            repl_config_ = config;
-        };
+        Console(const uint32_t task_stack_size = 4096, const BaseType_t task_priority = 2, int max_cmdline_len = 256, int max_cmdline_args = 8) : task_priority_(task_priority), task_stack_size_(task_stack_size), max_cmdline_len_(max_cmdline_len), max_cmdline_args_(max_cmdline_args){};
 
         ~Console()
         {
+            vTaskDelete(task_);
             end();
         }
 
         /**
          * @brief Register the given command, using the raw ESP-IDF structure.
-         * 
+         *
          * @param cmd The command that should be registered
          * @return Return true, if the registration was successfull, false if not.
          */
@@ -52,7 +54,8 @@ namespace ESP32Console
             log_v("Registering new command %s", cmd->command);
 
             auto code = esp_console_cmd_register(cmd);
-            if(code != ESP_OK) {
+            if (code != ESP_OK)
+            {
                 log_e("Error registering command (Reason %s)", esp_err_to_name(code));
                 return false;
             }
@@ -62,20 +65,20 @@ namespace ESP32Console
 
         /**
          * @brief Register the given command
-         * 
+         *
          * @param cmd The command that should be registered
          * @return true If the command was registered successful.
          * @return false If the command was not registered because of an error.
          */
         bool registerCommand(const ConsoleCommandBase &cmd)
-        {   
-            auto c = cmd.toCommandStruct(); 
+        {
+            auto c = cmd.toCommandStruct();
             return registerCommand(&c);
         }
 
         /**
          * @brief Registers the given command
-         * 
+         *
          * @param command The name under which the command can be called (e.g. "ls"). Must not contain spaces.
          * @param func A pointer to the function which should be run, when this command is called
          * @param help A text shown in output of "help" command describing this command. When empty it is not shown in help.
@@ -83,7 +86,7 @@ namespace ESP32Console
          * @return true If the command was registered successful.
          * @return false If the command was not registered because of an error.
          */
-        bool registerCommand(const char *command, esp_console_cmd_func_t func, const char* help, const char* hint = "")
+        bool registerCommand(const char *command, esp_console_cmd_func_t func, const char *help, const char *hint = "")
         {
             const esp_console_cmd_t cmd = {
                 .command = command,
@@ -106,14 +109,20 @@ namespace ESP32Console
          *
          * @param prompt
          */
-        void setPrompt(const char *prompt) { repl_config_.prompt = prompt; };
+        void setPrompt(const char *prompt) { prompt_ = prompt; };
+
+        void setHistoryMaxLength(uint16_t max_length)
+        {
+            max_history_len_ = max_length;
+            linenoiseHistorySetMaxLen(max_length);
+        }
 
         /**
          * @brief Enable saving of command history, which makes history persistent over resets. SPIFF need to be enabled, or you need to pass the filename to use. Must be called before begin() to work!
          *
          * @param history_save_path The file which will be used to save command history
          */
-        void enablePersistentHistory(const char *history_save_path = "/data/.history.txt") { repl_config_.history_save_path = history_save_path; };
+        void enablePersistentHistory(const char *history_save_path = "/data/.history.txt") { history_save_path_ = history_save_path; };
 
         void begin(int baud, int rxPin = -1, int txPin = -1, uint8_t channel = 0);
 
