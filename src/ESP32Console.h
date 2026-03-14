ceb31360615181f981e7f4196984c0cb49fe008c
Jan Böhmer <mail@jan-boehmer.de>
2026-03-14 13:26:41 +0100

    Bumped version to 1.3.0

--- src/ESP32Console.h
+++ src/ESP32Console.h
@@ -4,7 +4,7 @@
 #error This library depends on ESP-IDF and only works on ESP32!
 #endif
 
-#define ESP32CONSOLE_VERSION "1.2.2"
+#define ESP32CONSOLE_VERSION "1.3.0"
 
 #include "ESP32Console/Console.h"
 #include "ESP32Console/ConsoleCommand.h"
