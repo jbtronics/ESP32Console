a0be11a6cbc706f83f7607c971c9822faff3d89b
Jan Böhmer <mail@jan-boehmer.de>
2026-03-14 13:25:19 +0100

    Improve default max_cmdline_args from 8 to 32
    This should help with issues like in #18

--- src/ESP32Console/Console.h
+++ src/ESP32Console/Console.h
@@ -38,7 +38,7 @@ namespace ESP32Console
         /**
          * @brief Create a new ESP32Console with the default parameters
          */
-        Console(const uint32_t task_stack_size = 4096, const BaseType_t task_priority = 2, int max_cmdline_len = 256, int max_cmdline_args = 8) : task_priority_(task_priority), task_stack_size_(task_stack_size), max_cmdline_len_(max_cmdline_len), max_cmdline_args_(max_cmdline_args){};
+        Console(const uint32_t task_stack_size = 4096, const BaseType_t task_priority = 2, int max_cmdline_len = 256, int max_cmdline_args = 32) : task_priority_(task_priority), task_stack_size_(task_stack_size), max_cmdline_len_(max_cmdline_len), max_cmdline_args_(max_cmdline_args){};
 
         ~Console()
         {
